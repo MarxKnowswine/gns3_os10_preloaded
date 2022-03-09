e95c5aeec14726b8a6b506e5aed0af845e04aaa4
Marcello Savino <97905537+MarxKnowswine@users.noreply.github.com>
2022-03-09 10:50:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -86,8 +86,11 @@ done
 #rm os10-backup-restore/inventory
 #cp os10/inventory os10-backup-restore/
 sudo cp -r os10/ /home/gns3-user/
+sudo cp -r sonic/ /home/gns3-user/
 sudo chown -R gns3-user:gns3-user /home/gns3-user/os10
+sudo chown -R gns3-user:gns3-user /home/gns3-user/sonic
 sudo chmod -R 774 /home/gns3-user/os10
+sudo chmod -R 774 /home/gns3-user/sonic
 
 
 echo
